e79a10d309d37bb40300464c2e326ad1e2ea82da
gottaboy <gottaboy@github.com>
2020-02-04 21:06:37 +0800

    mac + CLion + redis5 本地调试/运行

---
+++ CMakeFiles/redis-server.dir/DependInfo.cmake
@@ -0,0 +1,96 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/minyi/CLionProjects/redis/src/adlist.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/adlist.c.o"
+  "/Users/minyi/CLionProjects/redis/src/ae.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/ae.c.o"
+  "/Users/minyi/CLionProjects/redis/src/ae_kqueue.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/ae_kqueue.c.o"
+  "/Users/minyi/CLionProjects/redis/src/anet.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/anet.c.o"
+  "/Users/minyi/CLionProjects/redis/src/aof.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/aof.c.o"
+  "/Users/minyi/CLionProjects/redis/src/bio.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/bio.c.o"
+  "/Users/minyi/CLionProjects/redis/src/bitops.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/bitops.c.o"
+  "/Users/minyi/CLionProjects/redis/src/blocked.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/blocked.c.o"
+  "/Users/minyi/CLionProjects/redis/src/childinfo.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/childinfo.c.o"
+  "/Users/minyi/CLionProjects/redis/src/cluster.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/cluster.c.o"
+  "/Users/minyi/CLionProjects/redis/src/config.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/config.c.o"
+  "/Users/minyi/CLionProjects/redis/src/crc16.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/crc16.c.o"
+  "/Users/minyi/CLionProjects/redis/src/crc64.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/crc64.c.o"
+  "/Users/minyi/CLionProjects/redis/src/db.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/db.c.o"
+  "/Users/minyi/CLionProjects/redis/src/debug.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/debug.c.o"
+  "/Users/minyi/CLionProjects/redis/src/defrag.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/defrag.c.o"
+  "/Users/minyi/CLionProjects/redis/src/dict.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/dict.c.o"
+  "/Users/minyi/CLionProjects/redis/src/endianconv.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/endianconv.c.o"
+  "/Users/minyi/CLionProjects/redis/src/evict.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/evict.c.o"
+  "/Users/minyi/CLionProjects/redis/src/expire.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/expire.c.o"
+  "/Users/minyi/CLionProjects/redis/src/geo.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/geo.c.o"
+  "/Users/minyi/CLionProjects/redis/src/geohash.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/geohash.c.o"
+  "/Users/minyi/CLionProjects/redis/src/geohash_helper.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/geohash_helper.c.o"
+  "/Users/minyi/CLionProjects/redis/src/hyperloglog.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/hyperloglog.c.o"
+  "/Users/minyi/CLionProjects/redis/src/intset.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/intset.c.o"
+  "/Users/minyi/CLionProjects/redis/src/latency.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/latency.c.o"
+  "/Users/minyi/CLionProjects/redis/src/lazyfree.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/lazyfree.c.o"
+  "/Users/minyi/CLionProjects/redis/src/listpack.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/listpack.c.o"
+  "/Users/minyi/CLionProjects/redis/src/localtime.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/localtime.c.o"
+  "/Users/minyi/CLionProjects/redis/src/lolwut.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/lolwut.c.o"
+  "/Users/minyi/CLionProjects/redis/src/lolwut5.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/lolwut5.c.o"
+  "/Users/minyi/CLionProjects/redis/src/lzf_c.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/lzf_c.c.o"
+  "/Users/minyi/CLionProjects/redis/src/lzf_d.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/lzf_d.c.o"
+  "/Users/minyi/CLionProjects/redis/src/memtest.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/memtest.c.o"
+  "/Users/minyi/CLionProjects/redis/src/module.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/module.c.o"
+  "/Users/minyi/CLionProjects/redis/src/multi.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/multi.c.o"
+  "/Users/minyi/CLionProjects/redis/src/networking.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/networking.c.o"
+  "/Users/minyi/CLionProjects/redis/src/notify.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/notify.c.o"
+  "/Users/minyi/CLionProjects/redis/src/object.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/object.c.o"
+  "/Users/minyi/CLionProjects/redis/src/pqsort.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/pqsort.c.o"
+  "/Users/minyi/CLionProjects/redis/src/pubsub.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/pubsub.c.o"
+  "/Users/minyi/CLionProjects/redis/src/quicklist.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/quicklist.c.o"
+  "/Users/minyi/CLionProjects/redis/src/rand.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/rand.c.o"
+  "/Users/minyi/CLionProjects/redis/src/rax.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/rax.c.o"
+  "/Users/minyi/CLionProjects/redis/src/rdb.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/rdb.c.o"
+  "/Users/minyi/CLionProjects/redis/src/redis-check-aof.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/redis-check-aof.c.o"
+  "/Users/minyi/CLionProjects/redis/src/redis-check-rdb.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/redis-check-rdb.c.o"
+  "/Users/minyi/CLionProjects/redis/src/release.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/release.c.o"
+  "/Users/minyi/CLionProjects/redis/src/replication.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/replication.c.o"
+  "/Users/minyi/CLionProjects/redis/src/rio.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/rio.c.o"
+  "/Users/minyi/CLionProjects/redis/src/scripting.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/scripting.c.o"
+  "/Users/minyi/CLionProjects/redis/src/sds.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/sds.c.o"
+  "/Users/minyi/CLionProjects/redis/src/sentinel.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/sentinel.c.o"
+  "/Users/minyi/CLionProjects/redis/src/server.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/server.c.o"
+  "/Users/minyi/CLionProjects/redis/src/setproctitle.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/setproctitle.c.o"
+  "/Users/minyi/CLionProjects/redis/src/sha1.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/sha1.c.o"
+  "/Users/minyi/CLionProjects/redis/src/siphash.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/siphash.c.o"
+  "/Users/minyi/CLionProjects/redis/src/slowlog.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/slowlog.c.o"
+  "/Users/minyi/CLionProjects/redis/src/sort.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/sort.c.o"
+  "/Users/minyi/CLionProjects/redis/src/sparkline.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/sparkline.c.o"
+  "/Users/minyi/CLionProjects/redis/src/syncio.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/syncio.c.o"
+  "/Users/minyi/CLionProjects/redis/src/t_hash.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/t_hash.c.o"
+  "/Users/minyi/CLionProjects/redis/src/t_list.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/t_list.c.o"
+  "/Users/minyi/CLionProjects/redis/src/t_set.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/t_set.c.o"
+  "/Users/minyi/CLionProjects/redis/src/t_stream.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/t_stream.c.o"
+  "/Users/minyi/CLionProjects/redis/src/t_string.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/t_string.c.o"
+  "/Users/minyi/CLionProjects/redis/src/t_zset.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/t_zset.c.o"
+  "/Users/minyi/CLionProjects/redis/src/util.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/util.c.o"
+  "/Users/minyi/CLionProjects/redis/src/ziplist.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/ziplist.c.o"
+  "/Users/minyi/CLionProjects/redis/src/zipmap.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/zipmap.c.o"
+  "/Users/minyi/CLionProjects/redis/src/zmalloc.c" "/Users/minyi/CLionProjects/redis/CMakeFiles/redis-server.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps/linenoise"
+  "deps/hiredis"
+  "deps/lua/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/minyi/CLionProjects/redis/deps/lua/CMakeFiles/lua.dir/DependInfo.cmake"
+  "/Users/minyi/CLionProjects/redis/deps/linenoise/CMakeFiles/linenoise.dir/DependInfo.cmake"
+  "/Users/minyi/CLionProjects/redis/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/redis-cli.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/minyi/CLionProjects/redis/src/adlist.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/adlist.c.o"
+  "/Users/minyi/CLionProjects/redis/src/ae.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/ae.c.o"
+  "/Users/minyi/CLionProjects/redis/src/anet.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/anet.c.o"
+  "/Users/minyi/CLionProjects/redis/src/crc16.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/crc16.c.o"
+  "/Users/minyi/CLionProjects/redis/src/crc64.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/crc64.c.o"
+  "/Users/minyi/CLionProjects/redis/src/dict.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/dict.c.o"
+  "/Users/minyi/CLionProjects/redis/src/redis-cli.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/redis-cli.c.o"
+  "/Users/minyi/CLionProjects/redis/src/release.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/release.c.o"
+  "/Users/minyi/CLionProjects/redis/src/sds.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/sds.c.o"
+  "/Users/minyi/CLionProjects/redis/src/siphash.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/siphash.c.o"
+  "/Users/minyi/CLionProjects/redis/src/zmalloc.c" "/Users/minyi/CLionProjects/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/linenoise"
+  "../deps/hiredis"
+  "../deps/lua/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/minyi/CLionProjects/redis/cmake-build-debug/deps/linenoise/CMakeFiles/linenoise.dir/DependInfo.cmake"
+  "/Users/minyi/CLionProjects/redis/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
